1ff0c9d30b2705b888e826c8aed6dd66c051996c
Mel207 <angel1223vga@gmail.com>
2025-06-04 12:41:34 -0600

    script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// Navegación suave al hacer clic en los enlaces del menú
+document.querySelectorAll('.menu a').forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+      target.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
